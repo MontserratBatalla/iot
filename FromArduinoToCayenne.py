53ea07246bc263a61a72c01a164d8805a4b90c8d
MontserratBatalla <montsebatalla65@gmail.com>
2020-10-16 10:56:53 +0200

    Update FromArduinoToCayenne.py

--- FromArduinoToCayenne.py
+++ FromArduinoToCayenne.py
@@ -21,7 +21,7 @@ while True:
 	#	Humitat = read_serial.replace("Humidity:", "")
 	stringFromSerial = str (read_serial)	
 	if ("Temperature" in stringFromSerial):
-		Temperatura = read_serial[10:14]
+		Temperatura = read_serial[13:17]
 		client.virtualWrite(0,Temperatura)
 	if ("Humidity" in stringFromSerial):
 		Humitat = read_serial[10:14]
